cfaee9e499d04c2b4895c464d6d4776f677d05ad
yuangli <yuangli@localhost>
2021-01-26 12:04:37 -0500

    graph built

--- Map/src/graphs/Graph.java
+++ Map/src/graphs/Graph.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class Graph<E> {
+public class Graph<E, T> {
 	
 	HashMap<E, Vertex> vertices;
 	
@@ -15,71 +15,94 @@ public class Graph<E> {
 		vertices.put(info, new Vertex(info));
 	}
 	
-	public void connect(E info1, E info2) {
+	public void connect(E info1, E info2, T label) {
 		Vertex v1 = vertices.get(info1);
 		Vertex v2 = vertices.get(info2);
-		v1.neighbors.add(v2);
-		v2.neighbors.add(v1);
+		
+		Edge e = new Edge(label, v1, v2);
+		v1.neighbors.add(e);
+		v2.neighbors.add(e);
+	}
+	
+	private class Edge {
+		T label;
+		Vertex v1, v2;
+		
+		public Edge(T label, Vertex v1, Vertex v2){
+			this.label = label; this.v1 = v1; this.v2 = v2;
+		}
+		
+		public Vertex getneighbor(Vertex v){
+			if(v.info.equals(v1.info)){
+				return v2;
+			}else{
+				return v1;
+			}
+		}
 	}
 
 	
-	
 	private class Vertex {
 		E info;
-		HashSet<Vertex> neighbors;
+		HashSet<Edge> neighbors;
 		
 		public Vertex(E info) {
 			this.info = info;
-			neighbors = new HashSet<Vertex>();
+			neighbors = new HashSet<Edge>();
 		}
+		
 	}
 	
 	
-	public ArrayList<E> search(E start, E target) {
+	public ArrayList<Object> search(E start, E target) {
 		
 		ArrayList<Vertex> toVisit = new ArrayList<Vertex>();
 		toVisit.add(vertices.get(start));
 		HashSet<Vertex> visited = new HashSet<Vertex>();
 		visited.add(vertices.get(start));
 		
-		HashMap<Vertex, Vertex> leadsTo = new HashMap<Vertex, Vertex>();
+		HashMap<Vertex, Edge> leadsTo = new HashMap<Vertex, Edge>();
 		
 		while (!toVisit.isEmpty()) {
 			
 			Vertex curr = toVisit.remove(0);
 			
-			for (Vertex neighbor : curr.neighbors) {
+			for (Edge e : curr.neighbors) {
 				
-				if (visited.contains(neighbor)) continue;
+				Vertex v = e.getneighbor(curr);
 				
-				leadsTo.put(neighbor, curr);
+				if (visited.contains(v)) continue;
 				
-				if (neighbor.info.equals(target)) {		
-					return backtrace(neighbor, leadsTo);
+				leadsTo.put(v, e);
+				
+				if (v.info.equals(target)) {		
+					return backtrace(v, leadsTo);
 				}else {
-					toVisit.add(neighbor);
-					visited.add(neighbor);
+					toVisit.add(v);
+					visited.add(v);
 				}
 			}
 		}
 		return null;
 	}
 	
-	public ArrayList<E> backtrace(Vertex target, HashMap<Vertex, Vertex> leadsTo) {
+	public ArrayList<Object> backtrace(Vertex target, HashMap<Vertex, Edge> leadsTo) {
 		
 		Vertex curr = target;
-		ArrayList<E> path = new ArrayList<E>();
-		
-		while (curr != null) {
-			path.add(0, curr.info);
-			curr = leadsTo.get(curr);
+		ArrayList<Object> path = new ArrayList<Object>();
+		path.add(0, curr.info);
+
+		while (leadsTo.get(curr) != null) {
+			path.add(0, leadsTo.get(curr).getneighbor(curr).info);
+			curr = leadsTo.get(curr).getneighbor(curr);
 		}
+		System.out.print(path);
 		return path;	
 	}
 	
 	public static void main(String[] args) {
 		
-		Graph<String> g = new Graph<String>();
+		Graph<String, String> g = new Graph<String, String>();
 		
 		g.addVertex("one");
 		g.addVertex("two");
@@ -88,11 +111,11 @@ public class Graph<E> {
 		g.addVertex("five");
 		g.addVertex("na");
 		
-		g.connect("one","two");
-		g.connect("two", "three");
-		g.connect("four", "three");
-		g.connect("five", "four");
+		g.connect("one","two", "g");
+		g.connect("two", "three", "h");
+		g.connect("four", "three", "a");
+		g.connect("five", "four", "b");
 		
-		System.out.println(g.search("five", "one"));
+		g.search("one", "two");
 	}
 }
--- Map/src/graphs/KevinBaconGame.java
+++ Map/src/graphs/KevinBaconGame.java
@@ -1,12 +1,124 @@
 package graphs;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 class KevinBaconGame {
 	HashMap<Integer, String> actorName = new HashMap<Integer, String>();
 	HashMap<Integer, String> movieName = new HashMap<Integer, String>();
+	Graph KBGgraph = new Graph();
 	
-	public KevinBaconGame(){
+	public KevinBaconGame() throws IOException{
+		buildCodetoNameMaps();
+		buildVertices();
+		connectVertices();
+		
+	}
+	
+	private boolean buildCodetoNameMaps() throws IOException{
+		//reading the actors name file generating code to actor name map
+				BufferedReader in = new BufferedReader(new FileReader("resource/actors.txt"));
+				String code = "";
+				String name = "";
+				int control;
+				for(String line = in.readLine(); line != null; line = in.readLine()){
+					if(line.charAt(0) == 65279){line = line.substring(1);}
+					//control for reading code or name, turns to 1 once read ~
+					control = 0;
+					for(int i = 0; i < line.length(); i++){
+						if(line.charAt(i) == '~'){ control = 1; continue;}
+						if(control == 0){code += line.charAt(i);}
+						else{name += line.charAt(i);}
+					}
+					actorName.put(Integer.parseInt(code), name);
+					code = "";
+					name = "";			
+				}
+				in.close();
+				System.out.println("actorName size: " + actorName.size());
+				
+		//reading the movie name file generating code to movie name map
+				BufferedReader in1 = new BufferedReader(new FileReader("resource/movies.txt"));
+				for(String line = in1.readLine(); line != null; line = in1.readLine()){
+					if(line.charAt(0) == 65279){line = line.substring(1);}
+					//control for reading code or name, turns to 1 once read ~
+					control = 0;
+					for(int i = 0; i < line.length(); i++){
+						if(line.charAt(i) == '~'){ control = 1; continue;}
+						if(control == 0){code += line.charAt(i);}
+						else{name += line.charAt(i);}
+					}
+					movieName.put(Integer.parseInt(code), name);
+					code = "";
+					name = "";			
+				}
+				in.close();
+				System.out.println("movieName size: " + movieName.size());
+				return true;
+	}
+	
+	private boolean buildVertices(){
+		//creating a set of vertices with each one holding name of one actor
+		for (String name : actorName.values()){
+			KBGgraph.addVertex(name);
+		}
+		return true;
+	}
+	
+	private boolean connectVertices() throws IOException{
+		String movieCode = "";
+		int connection = 0;
+		String lastMovieCode = "";
+		String actorCode = "";
+		//reading the actors name file generating code to actor name map
+		BufferedReader in = new BufferedReader(new FileReader("resource/movie-actors.txt"));
+		int control;
+		//Array list storing names of all actors in one movie. 
+		//Cleared once moved on to another movie.
+		ArrayList<Integer> actorList = new ArrayList<Integer>();
+		
+		for(String line = in.readLine(); line != null; line = in.readLine()){
+			if(line.charAt(0) == 65279){line = line.substring(1);}
+			//control for reading code or name, turns to 1 once read ~
+			control = 0;
+			for(int i = 0; i < line.length(); i++){
+				if(line.charAt(i) == '~'){ control = 1; continue;}
+				if(control == 0){movieCode += line.charAt(i);}
+				else{actorCode += line.charAt(i);}
+			}
+			//check if the loop has iterated to a new movie
+			if(lastMovieCode.length()==0 || lastMovieCode.equals(movieCode)){
+				lastMovieCode = movieCode;
+				actorList.add(Integer.parseInt(actorCode));
+				movieCode = "";
+				actorCode = "";	
+			}else{
+				lastMovieCode = movieCode;
+				//connect all actors in in the same movie
+				for(int i = 0; i < actorList.size(); i++){
+					for( int j = i+1; j < actorList.size(); j++){
+						KBGgraph.connect(actorName.get(actorList.get(i)),
+								actorName.get(actorList.get(j)), movieName.get(movieCode));
+						connection++;
+					}
+				}
+				actorList.clear();
+				actorList.add(Integer.parseInt(actorCode));
+				movieCode = "";
+				actorCode = "";	
+			}		
+		}
+		in.close();
+		System.out.println("connections established: " + connection);
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		KevinBaconGame myGame = new KevinBaconGame();
 		
 	}
 }
